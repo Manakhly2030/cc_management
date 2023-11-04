8677814840f8023d3b935c9967c7990d3283740a
Jeans K. Real <jkrb7@hotmail.com>
2023-11-03 22:03:29 -0600

    Adding color indicators

--- cc_management/credit_card_management/doctype/credit_card_tool/credit_card_tool.js
+++ cc_management/credit_card_management/doctype/credit_card_tool/credit_card_tool.js
@@ -8,11 +8,23 @@ frappe.ui.form.on("Credit Card Tool", {
 		frm.events.calculate_virtual_fields(frm); // TODO: const capable_cards = frm.doc.credit_cards.filter(card => card.credit_limit >= frm.doc.amount);
 
 		const format_as_counter = (date) => {
+			// TODO: Change this to calculate_virtual_fields
 			const diff_days = moment(date).diff(moment().startOf('day'), 'days');
-			return (diff_days === 0) ? 'Today' : (diff_days === 1) ? 'Tomorrow' : `In ${diff_days} days`;  // .calendar()
+
+			// acts like a moment().calendar()
+			if (diff_days === 0) {
+				return `<b>Today</b>`;
+			} else if (diff_days === 1) {
+				return `<b>Tomorrow</b>`;
+			} else {
+				let color = (diff_days <= 10) ? 'darkred' : (diff_days <= 15) ? 'orange' : (diff_days <= 30) ? 'green' : 'blue';
+
+				return `<b style='color: ${color}'>En ${diff_days} dias</b>`;
+			}
 		};
 
-		new DataTable(frm.fields_dict['results'].wrapper, {
+		// TODO: Color Row based on inputs!
+		let datatable = new DataTable(frm.fields_dict['results'].wrapper, {
 			layout: 'fluid',
 			data: frm.doc.credit_cards,
 			columns: [
@@ -24,8 +36,7 @@ frappe.ui.form.on("Credit Card Tool", {
 			]
 		});
 
-		// TODO: Color Row based on inputs!
-		//datatable.style.setStyle(`.dt-row-0 > .dt-cell`, {'background-color': 'red !important'});
+		datatable.sortColumn('5', 'desc'); // Sort by Days until pay_day_date
 	},
 
 	calculate_virtual_fields(frm) {
@@ -43,6 +54,8 @@ frappe.ui.form.on("Credit Card Tool", {
 
 			// Format Using frappe.defaultDateFormat. This way we can sort in tables
 			[card.cut_off_date, card.pay_day_date] = [cut_off_date.format(), pay_day_date.format()];
+
+			// TODO: Calculate here the days_until_cut and days_until_pay. not in the format function
 		});
 	}
 });
