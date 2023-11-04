50a6604c9aef2fc5a4b49a89b0b695809716df17
Jeans K. Real <jkrb7@hotmail.com>
2023-11-03 23:01:23 -0600

    Adding color indicators

--- cc_management/credit_card_management/doctype/credit_card_tool/credit_card_tool.js
+++ cc_management/credit_card_management/doctype/credit_card_tool/credit_card_tool.js
@@ -8,8 +8,7 @@ frappe.ui.form.on("Credit Card Tool", {
 		frm.events.calculate_virtual_fields(frm); // TODO: const capable_cards = frm.doc.credit_cards.filter(card => card.credit_limit >= frm.doc.amount);
 
 		const format_as_counter = (date) => {
-			// TODO: Change this to calculate_virtual_fields
-			const diff_days = moment(date).diff(moment().startOf('day'), 'days');
+			const diff_days = moment(date).diff(moment().startOf('day'), 'days');  // TODO: Change this to calculate_virtual_fields
 
 			// acts like a moment().calendar()
 			if (diff_days === 0) {
@@ -17,7 +16,7 @@ frappe.ui.form.on("Credit Card Tool", {
 			} else if (diff_days === 1) {
 				return `<b>Tomorrow</b>`;
 			} else {
-				let color = (diff_days <= 10) ? 'darkred' : (diff_days <= 15) ? 'orange' : (diff_days <= 30) ? 'green' : 'blue';
+				let color = (diff_days <= 10) ? 'darkred' : (diff_days <= 15) ? 'orange' : (diff_days <= 25) ? 'blue' : 'green';
 
 				return `<b style='color: ${color}'>En ${diff_days} dias</b>`;
 			}
@@ -28,11 +27,12 @@ frappe.ui.form.on("Credit Card Tool", {
 			layout: 'fluid',
 			data: frm.doc.credit_cards,
 			columns: [
+				{id: 'bank', name: 'Banco', editable: false, dropdown: false, align: 'center'},
 				{id: 'card_name', name: 'Tarjeta', editable: false, dropdown: false, align: 'center', format: (val) => `<b>${val}</b>`},
 				{id: 'cut_off_date', name: 'Fecha de Corte', editable: false, format: (val) => moment(val).format('dddd D, MMMM')},
-				{id: 'cut_off_date', name: 'Dias para el Corte', editable: false, format: format_as_counter},
-				{id: 'pay_day_date', name: 'Proxima Fecha de Pago', editable: false, format: (val) => moment(val).format('dddd D, MMMM')},
-				{id: 'pay_day_date', name: 'Dias para el Pago', editable: false, format: format_as_counter},
+				{id: 'cut_off_date', name: 'Dias para el Corte', editable: false, align: 'center', format: format_as_counter},
+				{id: 'pay_day_date', name: 'Fecha de Pago', editable: false, format: (val) => moment(val).format('dddd D, MMMM')},
+				{id: 'pay_day_date', name: 'Dias para el Pago', editable: false, align: 'center', format: format_as_counter},
 			]
 		});
 
@@ -44,11 +44,11 @@ frappe.ui.form.on("Credit Card Tool", {
 			let cut_off_date = moment().date(card.cut_off_day); // Set the cuf_off_date to the right day
 			let pay_day_date = moment().date(card.pay_day);     // Set the pay_day_date to the right day
 
-			if (moment().date() > card.cut_off_day) { // Check if today is after the cut_off_day
+			if (card.cut_off_day < moment().date()) { // Check if today is after the cut_off_day
 				cut_off_date.add(1, 'months');
 			}
 
-			if (cut_off_date.date() >= card.pay_day) { // If cut_off_date is after the pay_day_date
+			if (pay_day_date <= cut_off_date) {
 				pay_day_date.add(1, 'months');
 			}
 
